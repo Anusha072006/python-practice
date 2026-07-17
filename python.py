4561e1e55bf86c813b22083730e7cc528a817c6d
Anusha072006 <mutukuruanusha@gmail.com>
2026-07-17 14:44:55 +0530

    Add practice questions for basic Python exercises

---
+++ python.py
@@ -0,0 +1,23 @@
+# today practice questions-->17-07-2026
+
+#question(1): print numbers from 1 to 10
+for i in range(1,10):
+  print(i)
+
+#question(2): print even numbers from 1 to 20
+for i in range(1,20):
+  if i % 2==0:
+    print(i)
+
+#question(3): print the multiplication table of a number
+num=int(input("enter a number:")
+for i in range(1,11):
+        print(num,"x",i,"=",num*i)
+
+#question(4): find the sum of numbers from 1 to n
+num=int(input("enter n:")
+        total=0
+for i in range(1,n+1):
+  print("sum=",total)
+
+
