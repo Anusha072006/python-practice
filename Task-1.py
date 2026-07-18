af44f4bb37b4e9774023224d54d9efc7776f4d9b
Anusha072006 <mutukuruanusha@gmail.com>
2026-07-18 22:56:05 +0530

    Implement math and random functions in Task-1.py
    This script demonstrates various math and random functions, including ceiling, power, square root calculations, and random number generation.

---
+++ Task-1.py
@@ -0,0 +1,41 @@
+# Task-1: today topics are math function and random function
+--------------module function--------------------------------
+#Find the smallest integer greater than or equal to 9.99.
+print(math.ceil(9.99))
+#Calculate 5 raised to the power 3.
+print(math.pow(5,3))
+#claculate 10 raised to the power 4
+print(math.pow(10,4))
+#Find the square root of 49.
+print(math.sqrt(49))
+#Find the square root of 225
+print(math.sqrt(225))
+#Use math.copysign() to give the number 15 the sign of -8.
+a=15
+b=-8
+print(math.copysign(a,b))
+#Use math.copysign() to give the number -25 the sign of 12.
+a=-25
+b=12
+print(math.copysign(a,b))
+#Take a floating-point number as input and print its ceiling value.
+print(math.ceil(12.0))
+#Take two numbers as input and print the result of math.pow()
+print(math.pow(11,20))
+#Take a number as input and print its square root.
+print(math.sqrt(6))
+#Take two numbers as input and print the result  of math.copysign()
+a=20
+b=-1
+print(math.copysign(a,b))
+-----------------random function--------------------------------
+#Generate a random decimal number between 0 and 1.
+print(random.random())
+# Generate a random integer between 1 and 10.
+print(random.random())
+#Generate a random integer between 50 and 100.
+print(random.random())
+#Generate a random number from 0 to 20 with a step of 2.
+print(random.random())
+#Generate a random floating-point number between 5 and 10.
+print(random.uniform(5,10))
