dc51ab9708a5d03ef84e9bbf4a5c1291f91cfd7f
Anusha072006 <mutukuruanusha@gmail.com>
2026-07-22 15:18:16 +0530

    Add Task-3.py with assignment and type conversion examples

---
+++ Task-3.py
@@ -0,0 +1,19 @@
+# TODAY TASK
+1.QUESTION: Using an assignment operator, output the remainder when length is divided by 3.
+length = 11
+length %=3
+print(length)
+
+2.QUESTION:Rearrange this code so that the output is 5.
+a=3
+b=1
+sum=a+b
+sum=sum+b
+print(sum)
+
+#3.QUESTION:In the previous problems we learned how to convert a type of variable to another.
+Run the code in the editor to see how the values change upon converting the type of the variable.
+a = 5
+print(str(a))
+print(bool(a))
+print(float(a))
