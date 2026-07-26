c44ac44a28379c1fb9dfa66e1d2502b2185cd68c
Anusha072006 <mutukuruanusha@gmail.com>
2026-07-26 20:47:37 +0530

    Add multiple programming exercises in Task-4.py

---
+++ Task-4.py
@@ -0,0 +1,60 @@
+1.QUESTION: Using an assignment operator, output the remainder when length is divided by 3.
+length = 11
+length %=3
+print(length)
+------------------------------------------------------------------------------------------
+2.QUESTION: You are given two variables height1 and height2 - use relational operators to output 
+height1 = 15
+height2 = 13
+print(True);
+--------------------------------------------------------------------------------------------
+3.QUESTION: assuming that the voting age is 18 i.e., a person's age has to be greater than or equal to 18 to vote.
+age = 20
+print(True);
+-------------------------------------------------------------------------------------------------------
+4.QUESTION: You are allowed to enter the waterpark only if your height is between 5 and 10.
+Apply the condition and output True or False as applicable.
+height = 15
+print(True);
+----------------------------------------------------------------------------------------------------------
+5.QUESTION: You are given a character in the variable name.
+There is a special rule in Chef town - You will get special treatment if your name is C or A.
+name = 'D'
+print(False);
+----------------------------------------------------------------------------------------------------------
+6.QUESTION:Create a string variable named fruit and assign the value Apple to it.
+Output fruit to the console.
+fruit='Apple'
+print(fruit);
+------------------------------------------------------------------------------------------------------------
+7.QUESTION: Declare two variables x and y
+Assign the values Hello to variable x and World to variable y.
+Output x and y to the console. Don't forget to add a space in between them.
+Your output will be Hello World
+x='Hello'
+y='World'
+print(x,y);
+--------------------------------------------------------------------------------------------------------------
+8.QUESTION: Write a program to print the word tenet 100 times
+print('tenet'* 100);
+---------------------------------------------------------------------------------------------------------------
+9.QUESTION: You want to output 2569
+Try running the given code given in the IDE as it is - it will give a Compilation error
+Fix the error so that both the variables become strings and the output is 2569
+string1 = "25"
+string2 = "69"
+print(string1 + string2)
+-----------------------------------------------------------------------------------------------------------------
+10.QUESTION: Create a variable txt and assign it the text NumeroTres
+Use the len() function to output to the console the number of characters in txt
+txt='NumeroTres'
+print(len(txt));
+-------------------------------------------------------------------------------------------------------------------
+11.QUESTION: You are given a string variable mixed_string with the value 'Am I DreAmIng'. Your task is to:
+Convert the string to all lowercase and print the result.
+Convert the string to all uppercase and print the result.
+mixed_string = 'Am I DreAmIng'
+print(mixed_string.lower())
+print(mixed_string.upper())
+----------------------------------------------------------------------------------------------------------------------
+
