a1dde93713fb932b62c4524d2bb08b46a1bf8ac1
Anusha072006 <mutukuruanusha@gmail.com>
2026-07-31 13:23:25 +0530

    Add string manipulation examples in Task-5.py

---
+++ Task-5.py
@@ -0,0 +1,38 @@
+TODAY'S TOPIC IS ----->STRING IN PYTHON
+---------------------------------------------------------------------------------------------
+#Create a string with your name and print it.
+str='anusha'
+print(str)
+-------------------------------------------------------------------------------------------------
+#Find the length of a string.
+print(len(str))
+----------------------------------------------------------------------------------------------------
+#Print the first and last character of a string.
+str="python"
+print(str[0:6])
+-----------------------------------------------------------------------------------------------------
+#Convert a string to uppercase.
+str="karthik"
+print(str.upper())
+----------------------------------------------------------------------------------------------------
+#Convert a string to lowercase.
+print(str.lower())
+-------------------------------------------------------------------------------------------------------
+#Remove leading and trailing spaces from a string.  
+str="   hello python   "
+print(str)
+-------------------------------------------------------------------------------------------------------
+#Replace one word with another in a string.
+text=" I like python"
+print(text.replace('python','java'))
+----------------------------------------------------------------------------------------------------------
+#Check whether a string starts with a given letter.
+str="gopi priya"
+print(str.startswith('g'))
+-----------------------------------------------------------------------------------------------------
+#Check whether a string ends with a given word.
+print(str.endswith('a'))
+---------------------------------------------------------------------------------------------------
+#Count how many times a character appears in a string.
+print(str.count('i'))
+
