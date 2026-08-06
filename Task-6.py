f958ac4cc5da3a5c756634e88178b913cdb62ca9
Anusha072006 <mutukuruanusha@gmail.com>
2026-08-06 11:09:47 +0530

    Add Python exercises for basic programming concepts
    This script contains various Python exercises demonstrating basic programming concepts such as area and perimeter calculations, variable assignments, and list operations.

---
+++ Task-6.py
@@ -0,0 +1,53 @@
+1.QUESTION: For any rectangle, the formula for area is length * breadth.
+The formula for perimeter is 2 * (length + breadth).
+print( 11*13)         
+print(2* (11+ 13))
+------------------------------------------------------------------------------------------------------------------------------------------
+2.QUESTION: You have to output the text:
+print(7, "plus", 3, "equals", 10)
+-------------------------------------------------------------------------------------------------------------------------------------------
+3.QUESTION: A variable is like a labelled box where you can store data. Imagine you have a box labeled "age" and you put the number 25 in it. In Python, you would do this by writing:
+age = 25
+print(age)
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+4.QUESTION:Write a program which does the following
+There is a variable named number having value 20 in the editor.
+Use the print command to output the value of (number - 1).
+number = 20
+print(number-1)
+------------------------------------------------------------------------------------------------------------------------------------------------------
+5.QUESTION: There is some code written in the editor to print Code Chef.
+But the variable names are not following the rule. Can you spot the mistake and fix it?
+first_name = "Code"
+last_name= "Chef"
+print(first_name,last_name)
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+6.QUESTION:Find out and display the area of a rectangle of sides 45 and 76 respectively.
+Declare variables length, width and area and assign the relevant values to them
+Output the value of variable area
+length=45
+-----------------------------------------------------------------------------------------------------------------------------------------------
+7.QUESTION: write program from Largest numbers
+numbers=[10,20,30,40,50]
+largest=max(numbers)
+print(largest)
+------------------------------------------------------------------------------------------------------------------------------------------------
+8.QUESTION: write the program for smallest numbers
+smallest=max(numbers)
+print("smallest element:",smallest)
+------------------------------------------------------------------------------------------------------------------------------------        
+9.QUESTION: write the program for second largest value
+numbers=[12,45,7,89,23,89]
+numbers.sort()
+print("second largest number:", numbers[-4])
+------------------------------------------------------------------------------------------------------------------------------------------------
+10.QUESTION: write a program for remove the duplicate elements from a list
+numbers=[1,23,45,78,9,4,4]
+numbers=list(set(numbers))
+print(numbers)
+---------------------------------------------------------------------------------------------------------------------------------------------------
+11.QUESTION: wite a program for area of rectangle
+width=76
+area=length* width 
+print(area)
+--------------------------------------------------------------------------------------------------------------------------------------------------------
