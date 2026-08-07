cec6cdbcd5bccbb1f8d7fcafe54fa9e64d3caed2
Anusha072006 <mutukuruanusha@gmail.com>
2026-08-07 13:15:02 +0530

    Add Task-7.py with circle area and string concatenation

---
+++ Task-7.py
@@ -0,0 +1,16 @@
+1.QUESTION: Write a program which does the following:
+Find the area of a circle whose radius is 8.9. Take pi = 3.14
+Declare variables radius , pi and area and assign the relevant values to them
+Output the area, you don't need to output any other text.
+radius=8.9
+pi=3.14
+area=pi* radius*radius
+print(area)
+---------------------------------------------------------------------------------------------------------------------------------------
+2.QUESTION: Write a program which does the following
+Declare two variables a and b
+Assign Learning to a and is fun! to b
+a='Learning'
+b='is fun'
+print(a+b)
+---------------------------------------------------------------------------------------------------------------------------------------------
