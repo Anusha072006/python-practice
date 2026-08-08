132c227bd254098ab9ccd89b9cf38cae485b2585
Anusha072006 <mutukuruanusha@gmail.com>
2026-08-08 15:31:21 +0530

    Create Task-8.py

---
+++ Task-8.py
@@ -0,0 +1,23 @@
+1.QUESTION: Find the area of a square of side 14cm.
+Also, find the cost of painting the square if the paint costs is 7$ per cm 
+Initialize the following variables in your program and use them in the computation
+s to store the length of side the square
+area to store the area of the square
+cost to store the cost of painting
+s=14
+area=s *s
+cost=area*7
+print(area)
+print(cost)
+-----------------------------------------------------------------------------------------------------------------------------------------------
+2.QUESTION: Display the sum and product of 2 integers: -50 & 40.
+Initialize the following variables in your program and use them in the computation
+Variable a and assign it the value -50
+Variable b assign it the value 40
+Variables total & product - assign the relevant values to them
+a=-50
+b=40
+total=a+b
+product=a*b
+print(total,product)
+---------------------------------------------------------------------------------------------------------------------------------------------------
