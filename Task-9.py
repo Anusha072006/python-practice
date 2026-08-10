2732c4f7ee4247f048a825d2276a5a6e80974eb9
Anusha072006 <mutukuruanusha@gmail.com>
2026-08-10 15:40:58 +0530

    Add input handling for string and integers

---
+++ Task-9.py
@@ -0,0 +1,19 @@
+1.QUESTION:  Write a code to accept a string as input and output the same.
+Some portion of the code is already written for you.
+You need to replace the _ (underscores) with correct code in the editor.
+Once done, click Submit.
+S =input()
+print(S)
+-----------------------------------------------------------------------------------------------------------------------------------
+2.QUESTION: Let us define a variable N
+N that accepts integer inputs.
+Output the same variable 
+To convert the input into an integer,we use the int() function.
+N = int(input())
+print(N)
+--------------------------------------------------------------------------------------------------------------------------------------
+3.QUESTION: Accept 3 space separated integers given in a line into 3 variables - A, B and C
+Print them out to a single line on the console 
+A, B, C = map(int,input().split())
+print(A, B, C)
+-----------------------------------------------------------------------------------------------------------------------------------------
