17893e9d3f1edf4b0b2945488bc36318331a9393
Anusha072006 <mutukuruanusha@gmail.com>
2026-08-30 22:24:07 +0530

    Implement various mathematical and greeting functions

---
+++ Task-18.py
@@ -0,0 +1,45 @@
+#FUNCTION:
+def cube(num):
+    return (num**2) *num;
+print(cube(3))
+1.QUESTION: Create a function greet() that prints:
+def greet():
+    print("Hello,welcome to python")
+greet()
+------------------------------------------------------------------------------------------------------
+2.QUESTION:Create a function welcome(name) that prints: 
+def greet(name):
+    print("welcome" ,"Anu")
+greet("Anu")
+-------------------------------------------------------------------------------------------------------------
+3.QUESTION: Create a function add(a, b) that returns the sum of two numbers.
+def add(a,b):
+    return a+b
+number=add(10,20)
+print(number)
+--------------------------------------------------------------------------------------------------------------------
+4.QUESTION: Create a function subtract(a, b) that returns the difference.
+def sub(a,b):
+    return a-b
+number=sub(20,10)
+print(number)
+-----------------------------------------------------------------------------------------------------------------------------------------
+5.QUESTION: Create a function multiply(a, b) that returns the multiplication.
+def mul(a,b):
+    return a*b
+number=mul(10,20)
+print(number)
+-------------------------------------------------------------------------------------------------------------------------------------------
+6.QUESTION: Create a function divide(a, b) that returns the division.
+def div(a,b):
+    return a/b
+number=div(4,3)
+print(number)
+----------------------------------------------------------------------------------------------------------------------------------------------
+7.QUESTION:Create a function check_even(num) that prints whether a number is Even or Odd.
+def check_even(num):
+  if num%2==0:
+    print("even")
+  else:
+    print("odd")
+check_even(5)
